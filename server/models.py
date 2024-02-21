e805d087bfba0ea31cada83e4f67c365ada52666
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-02-20 19:38:25 -0500

    models.py started

--- server/models.py
+++ server/models.py
@@ -4,3 +4,41 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+
+    meal_plans = db.relationship('MealPlan', back_populates='user', cascade='all, delete-orphan')
+
+class Category(db.Model, SerializerMixin):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', back_populates='category')
+
+class Recipe(db.Model, SerializerMixin):
+    __tablename__ = 'recipes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    directions = db.Column(db.String)
+
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    category = db.relationship('Category', back_populates='recipes')
+
+    meal_plans = db.relationship('MealPlan', back_populates='recipe', cascade='all, delete-orphan')
+
+class MealPlan(db.Model, SerializerMixin):
+    __tablename__ = 'meal_plans'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
+
+    user = db.relationship('User', back_populates='meal_plans')
+    recipe = db.relationship('Recipe', back_populates='meal_plans')
